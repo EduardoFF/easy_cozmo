e850bd9205403efc5ae7a7282e9e1c0635847273
Eduardo Feo <eduardo@idsia.ch>
2018-09-09 09:23:35 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='mindcraft-treasure-hunt-cozmo',
       version='1.0',
-      packages=['mindcraft-treasure-hunt-cozmo'],
+      packages=['mindcraft_treasure_hunt_cozmo'],
       )
