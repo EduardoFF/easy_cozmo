6d406b2215e1751c12e908e65c15318f5bbc0cac
Eduardo Feo <eduardo@idsia.ch>
2018-09-22 07:14:09 +0200

    added template in examples/mindcraft.py

---
+++ examples/mindcraft.py
@@ -0,0 +1,20 @@
+"""
+Starting point for Mindcraft
+"""
+
+from mindcraft_treasure_hunt_cozmo import *
+
+def task1():
+    """ write here your code for task 1 """
+    
+    return
+
+def task2():
+    """ write here your code for task 2 """
+
+    return
+
+"""
+run your code in Cozmo - for task 2, replace the task1 word with task2
+"""
+run_program_with_viewer(task1)
