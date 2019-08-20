fd7eeef96ad45d7289d45c196fc64e3b9fc7c69f
Eduardo Feo <eduardo@idsia.ch>
2019-08-20 17:02:39 +0300

    installation using setup.py, preparing first release

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# mindcraft-treasure-hunt-cozmo
+===========
+easy_cozmo
+===========
+
+easy_cozmo provides a bunch of python wrappers for Anki Cozmo's SDK. You might
+find it most useful for educational tasks involving students with little CS
+background. This library was created for a series of workshops for high shool
+students in Qatar. The code has been used by over 1,000 students over one year.
+Typical usage often looks like this::
+
+    """
+    Makes cozmo saying "hello world"
+    """
+
+    from easy_cozmo import *
+
+    def cozmo_program():
+    	say("Hello world")
+
+    run_program_with_viewer(cozmo_program)
+
+
+Documentation
+===============
+
+
+`Take a look at the documentation <http://www.example.com/foo/bar>`_.
--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
-from distutils.core import setup
-setup(name='mindcraft-treasure-hunt-cozmo',
-      version='1.0',
-      packages=['mindcraft_treasure_hunt_cozmo'],
-      )
+from setuptools import setup
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+with open(os.path.join(thelibFolder, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='easy_cozmo',
+    version='1.0.1',
+    author='Eduardo Feo-Flushing',
+    author_email='efeoflus@andrew.cmu.edu',
+    packages=['easy_cozmo'],
+    url='http://github.com/EduardoFF/easy_cozmo',
+    license='LICENSE.txt',
+    description='Useful python wrappers for Cozmo SDK.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+)
