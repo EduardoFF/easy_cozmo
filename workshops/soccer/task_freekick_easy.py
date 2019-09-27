6ba03e8a30137ab4d067fb7f63260262a5cbd81b
Eduardo Feo <eduardo@idsia.ch>
2019-09-27 07:56:47 +0300

    Task freekick easy

---
+++ workshops/soccer/task_freekick_easy.py
@@ -0,0 +1,32 @@
+from easy_cozmo import *
+from cozmo.util import degrees, Pose, distance_mm, speed_mmps
+def cozmo_program():
+    if scan_for_left_post(360):
+        dleft = distance_to_left_post()
+        if scan_for_right_post(360):
+            dright = distance_to_right_post()
+            if dleft < dright: # Cozmo's on the left side
+                if align_ball_and_right_post():
+                    rotate_right(48.5)
+                    move_forward(6.6)
+                    rotate_left(90)
+                    say("Going to shoot")
+                    if kick():
+                        say("Goal!")
+                        show_happy()
+                    else:
+                        show_sad()
+            else:
+                if align_ball_and_left_post():
+                    rotate_left(48.5)
+                    move_forward(6.6)
+                    rotate_right(90)
+                    say("Going to shoot")
+                    if kick():
+                        say("Goal!")
+                        show_happy()
+                    else:
+                        show_sad()
+
+
+run_program_with_viewer(cozmo_program)
